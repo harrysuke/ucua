121aa2b993ad5014d043de3d5dec4ea360ac9800
Kairi Juri <khairij@gmail.com>
2022-05-05 17:16:48 +0800

    Initial Commit

---
+++ ucua/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ucua
+//
+//  Created by Kairi Juri on 05/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
